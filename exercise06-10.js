d844b9a96e4ea569dc89662e8710f1d1510a2176
Steve Larsen <slarsen-3@att.net>
2021-02-21 09:39:12 -0600

    required fields

--- exercise06-10.js
+++ exercise06-10.js
@@ -1,11 +1,37 @@
 document
   .getElementById("username")
-  .insertAdjacentHTML("beforebegin", '<label for="username">Username: </label>');
-  document
+  .insertAdjacentHTML(
+    "beforebegin",
+    '<label for="username">Username: </label>'
+  );
+document
   .getElementById("password")
-  .insertAdjacentHTML("beforebegin", '<label for="password">Password: </label>');
-  document
+  .insertAdjacentHTML(
+    "beforebegin",
+    '<label for="password">Password: </label>'
+  );
+document
   .getElementById("confirmPassword")
-  .insertAdjacentHTML("beforebegin", '<label for="confirmPassword">Confirm Password: </label>');
+  .insertAdjacentHTML(
+    "beforebegin",
+    '<label for="confirmPassword">Confirm Password: </label>'
+  );
 
-  
+const checkInput = (event) => {
+  const errorElement = Array.from(
+    event.target.parentNode.querySelectorAll("span")
+  );
+  if (event.target.value === "" && !errorElement.length) {
+    event.target.insertAdjacentHTML(
+      "afterend",
+      '<span class="text-danger">Required</span>'
+    );
+  }
+  if (errorElement && event.target.value !== "") {
+    errorElement.forEach((elem) => elem.remove());
+  }
+};
+
+document.getElementById("username").addEventListener("blur", checkInput);
+document.getElementById("password").addEventListener("blur", checkInput);
+document.getElementById("confirmPassword").addEventListener("blur", checkInput);
