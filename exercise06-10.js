5c8582a826f76bac63cd6ef32aa2040a37ffb1ff
Steve Larsen <slarsen-3@att.net>
2021-02-21 09:21:07 -0600

    regsitarion form add labels using js

---
+++ exercise06-10.js
@@ -0,0 +1,11 @@
+document
+  .getElementById("username")
+  .insertAdjacentHTML("beforebegin", '<label for="username">Username: </label>');
+  document
+  .getElementById("password")
+  .insertAdjacentHTML("beforebegin", '<label for="password">Password: </label>');
+  document
+  .getElementById("confirmPassword")
+  .insertAdjacentHTML("beforebegin", '<label for="confirmPassword">Confirm Password: </label>');
+
+  
